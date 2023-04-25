5149659faed6943cf9b1b3d0610ba28432831fd7
Mars Adeniji <adenijifara@gmail.com>
2023-04-25 12:03:35 +0300

    Fix betty errors

--- main.h
+++ main.h
@@ -6,11 +6,17 @@
 #include <limits.h>
 #include <stdio.h>
 int _printf(const char *format, ...);
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
 typedef struct format
 {
 	char *id;
 	int (*f)(va_list);
-}conversion;
+} conversion;
 int _print_c(va_list ap);
 int _print_s(va_list ap);
 int _strlen(char *s);
