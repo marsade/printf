1a96502326a3a788a60cda3ded232b58e59da4b7
justclarke <robertclarke29.rc@gmail.com>
2023-04-25 05:08:11 -0700

    Add hexadecimal and unsigned functions

--- _printf.c
+++ _printf.c
@@ -12,6 +12,8 @@ int _printf(const char *format, ...)
 		{"%d", _printf_d},
 		{"%i", _printf_i},
 		{"%b", _printf_b},
+		{"%u", _printf_u},
+		{"%x", _printf_x},
 	};
 	va_list args;
 	int i, j = 0, len = 0;
